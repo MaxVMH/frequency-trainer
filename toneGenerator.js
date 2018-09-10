730a7584be7e023206a2a8f1fbc8c44940ebb14e
MaxVMH <maximvanmeerhaeghe@me.com>
2018-09-10 12:27:35 +0200

    default difficulty mode set to easy
    default difficulty mode set to easy instead of normal because normal mode includes frequencies that are barely hearable on certain speakers.

--- toneGenerator.js
+++ toneGenerator.js
@@ -131,7 +131,7 @@ function getDifficultyMode() {
   let difficultyMode = urlParameters.get('mode');
   if(!difficultyMode)
   {
-    difficultyMode = 'normal';
+    difficultyMode = 'easy';
   }
   return difficultyMode;
 };
